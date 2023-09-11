8f7a54058955c9e18fd1a5d7a05d3e8f1695ea2e
CRosenauer <christopherrosenauer101@gmail.com>
2023-09-11 16:58:10 -0700

    Removed now unused resolution setting code

--- Assets/Scripts/Systems/ResolutionSystem.cs
+++ Assets/Scripts/Systems/ResolutionSystem.cs
@@ -7,7 +7,6 @@ public class ResolutionSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(854, 480, true, 60);
     }
 
     // Update is called once per frame
