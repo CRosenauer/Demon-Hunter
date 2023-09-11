6e7e7fbc9e74f0c0211c362a7a77bb435d556172
CRosenauer <christopherrosenauer101@gmail.com>
2023-09-11 14:23:32 -0700

    Fixed UI with varying resolutions

--- Assets/Scripts/Systems/ResolutionSystem.cs
+++ Assets/Scripts/Systems/ResolutionSystem.cs
@@ -7,7 +7,7 @@ public class ResolutionSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(640, 480, false, 60);
+        Screen.SetResolution(854, 480, true, 60);
     }
 
     // Update is called once per frame
