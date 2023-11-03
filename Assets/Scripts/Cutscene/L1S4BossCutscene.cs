0b91a2938b30dccb2fdca2f300a9f87a5534816f
CRosenauer <christopherrosenauer101@gmail.com>
2023-11-03 14:09:53 -0700

    Boss intro cutscene

---
+++ Assets/Scripts/Cutscene/L1S4BossCutscene.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L1S4BossCutscene : Cutscene
+{
+    [SerializeField] GameObject m_bossCultist;
+    [SerializeField] GameObject m_cultistLaugh;
+
+    protected override void CutsceneFn()
+    {
+        StartCoroutine(CutsceneCoroutine());
+    }
+
+    IEnumerator CutsceneCoroutine()
+    {
+        while (!m_player)
+        {
+            m_player = GameObject.FindGameObjectWithTag("Player");
+            yield return new WaitForEndOfFrame();
+        }
+
+        m_player.SendMessage("SetCutscene", true);
+        m_bossCultist.SendMessage("SetCutscene", true);
+        m_player.SendMessage("Move", Vector2.zero);
+
+        yield return new WaitForSeconds(1f);
+
+        Animator cultistAnimator = m_bossCultist.GetComponent<Animator>();
+        cultistAnimator.SetTrigger("Laugh");
+        PlaySound(m_cultistLaugh);
+        yield return new WaitForSeconds(3f);
+
+        m_player.SendMessage("SetCutscene", false);
+        m_bossCultist.SendMessage("SetCutscene", false);
+
+        m_bossCultist.SendMessage("Activate");
+    }
+
+    GameObject m_player;
+}
